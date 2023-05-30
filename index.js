34786d3b134470420d3be94cce30561d9dee782b
JOHNY VINCENT <108324077+JOHNYVINCENT1@users.noreply.github.com>
2023-05-30 11:24:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var randomNumber1 = Math.floor(Math.random() *6)+1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage ;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+
+// var randomNumber2 = Math.floor(Math.random()*6)+1;
+// var randomImageSource2 = "images/dice
+
+var randomNumber2 = Math.floor(Math.random() *6)+1;
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+var randomImageSource = "images/" + randomDiceImage ;
+var image2 = document.querySelectorAll("img")[1];
+image2 .setAttribute("src", randomImageSource);
+
+if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "player 2 wins🏆!";
+}else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "player 1 wins🏆!";
+
+}else{
+    document.querySelector("h1").innerHTML = "Draw, play again";
+
+}
